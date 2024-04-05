e1815ca6ab342c8d69c80f85cd7d1f24c89573c3
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-05 17:14:17 +0000

    п

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import {TonProofDemo} from "./components/TonProofDemo/TonProofDemo";
 function App() {
   return (
       <TonConnectUIProvider
-          manifestUrl="https://winstoumer.github.io/MiningBot/docs/tonconnect-manifest.json"
+          manifestUrl="https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json"
           uiPreferences={{ theme: THEME.DARK }}
           walletsListConfiguration={{
             includeWallets: [
