63283e65bb0bcc3d470096ac6cbfe723839072ae
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-10 09:27:27 +0000

    h

--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -54,16 +54,12 @@ const Task: React.FC = () => {
   }, []);
 
      const handleCopyLink = () => {
-    if (userData && userData.id) {
-      const userId = userData.id;
-      const referralLink = `https://t.me/minerweb3_bot?start=r_${userId}`;
-      navigator.clipboard.writeText(referralLink)
+    const userId = userData && userData.id ? userData.id : 'null';
+    const referralLink = `https://t.me/minerweb3_bot?start=r_${userId}`;
+    navigator.clipboard.writeText(referralLink)
         .then(() => alert('Link copied to clipboard'))
         .catch((error) => console.error('Error copying link: ', error));
-    } else {
-      console.error('User data is not available yet');
-    }
-  };
+};
 
     const telegramGroupUrl = 'https://t.me/notcoin';
     const instagramProfileUrl = 'https://www.instagram.com/winstoum/';
