3f671201fc25a20d8e5522c37f05f8387c944ff2
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-12 16:54:21 +0000

    jjj

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,7 +20,7 @@ const fetchMiner = async (userId: string, setMinerInfo: React.Dispatch<any>) => 
 
 const Home: React.FC = () => {
   const [userData, setUserData] = useState<any>(null);
-    const [timeMined, setTimeMined] = useState<any>(initialValue);
+    const [timeMined, setTimeMined] = useState<number>(0);
   const [coins, setCoins] = useState<number>(0);
   const [count, setCount] = useState<number>(0);
   const [minerInfo, setMinerInfo] = useState<any>({});
