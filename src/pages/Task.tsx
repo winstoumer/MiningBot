d39aa365d98c1e11e209aebd49b4f80837557b5a
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-14 20:55:27 +0000

    с

--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -74,7 +74,6 @@ const Task: React.FC = () => {
   const handleTaskCompletion = async (taskId: number, url: string) => {
     try {
       if (!userData) return;
-      localStorage.clear();
       const userId = userData.id.toString();
       const response = await fetch(`https://advisory-brandi-webapp.koyeb.app/api/completed_tasks`, {
         method: 'POST',
@@ -91,11 +90,29 @@ const Task: React.FC = () => {
           task.id === taskId ? { ...task, completed: true } : task
         )
       );
+
+      // Save completed tasks to localStorage
+      const completedTasks = JSON.parse(localStorage.getItem('completedTasks') || '[]');
+      const updatedCompletedTasks = [...completedTasks, taskId];
+      localStorage.setItem('completedTasks', JSON.stringify(updatedCompletedTasks));
+
       window.location.href = url;
     } catch (error) {
       console.error('Error completing task:', error);
     }
-  };
+};
+
+    useEffect(() => {
+    // Load completed tasks from localStorage
+    const completedTasks = JSON.parse(localStorage.getItem('completedTasks') || '[]');
+    // Update tasks state based on completed tasks
+    setTasks(prevTasks =>
+        prevTasks.map(task =>
+            completedTasks.includes(task.id) ? { ...task, completed: true } : task
+        )
+    );
+}, []);
+
 
   useEffect(() => {
     const loadScript = () => {
