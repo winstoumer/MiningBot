e75a0d0ce33c8ba62ffdf3c9653d3c375d735dbb
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-15 14:33:23 +0000

    nf

--- src/pages/Boost.tsx
+++ src/pages/Boost.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router'; // Попробуй использовать такой импорт
+import { useHistory } from 'react-router-dom';
 import './boost.scss';
 
 type TelegramUserData = {
