c14707c337b123d5c3bfebcf79d9992d56696804
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-15 14:35:13 +0000

    jd

--- src/pages/Boost.tsx
+++ src/pages/Boost.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
 import './boost.scss';
 
