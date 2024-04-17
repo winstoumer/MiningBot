0c9fcff4c1356a943a4a93f10b1f7c7218b6ff7a
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-17 21:34:04 +0000

    g

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,10 +1,11 @@
 import './navigation.scss';
 import React, { useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import PageComponent from '../components/PageComponent/PageComponent';
 
 export const Navigation = () => {
     
-    return <div className="bottom-navigation">
+    return <PageComponent><div className="bottom-navigation">
         <div className="navigation-block">
            <Link to="/">Mining</Link>
         </div>
@@ -18,4 +19,5 @@ export const Navigation = () => {
             <Link to="/market">Box</Link>
         </div>
     </div>
+    </PageComponent>
 };
