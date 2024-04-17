5daba4cd6b6cc2d5288bd04636957ceafb474138
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-17 21:50:35 +0000

    bh

--- src/components/PageComponent/PageComponent.tsx
+++ src/components/PageComponent/PageComponent.tsx
@@ -18,31 +18,32 @@ const PageComponent: React.FC<PageComponentProps> = ({ children }) => {
         if (window.Telegram && window.Telegram.WebApp) {
           window.Telegram.WebApp.setHeaderColor('#ffffff');
           window.Telegram.WebApp.expand();
+
+          // Set up the back button after the script is loaded and Telegram WebApp is available
+          const backButton = window.Telegram.WebApp.BackButton;
+          backButton.onClick(() => {
+            console.log('Back button clicked');
+            navigate(-1); // Use navigate with a negative number to go back
+          });
+
+          // Show or hide the back button based on the current path
+          if (window.location.pathname !== '/') {
+            backButton.show();
+          } else {
+            backButton.hide();
+          }
         }
       };
       document.body.appendChild(script);
     };
 
     loadScript();
-  }, []); 
 
-  useEffect(() => {
-    const backButton = window.Telegram.WebApp.BackButton;
-
-    if (window.location.pathname !== '/') {
-      backButton.show();
-    } else {
-      backButton.hide();
-    }
-
-    backButton.onClick(() => {
-      console.log('Back button clicked');
-      navigate(-1); // Используем navigate с отрицательным числом для перехода назад
-    });
-
-    // Очистка обработчика при размонтировании компонента
+    // Clean up the back button when the component is unmounted
     return () => {
-      backButton.hide();
+      if (window.Telegram && window.Telegram.WebApp) {
+        window.Telegram.WebApp.BackButton.hide();
+      }
     };
   }, [navigate]);
 
