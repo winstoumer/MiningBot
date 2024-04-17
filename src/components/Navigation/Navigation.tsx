64cc85656af212c4ff8ccd4eefac51775d5d45b3
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-17 09:27:46 +0000

    очо

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,8 +1,30 @@
 import './navigation.scss';
-import React from 'react';
-import { Link } from 'react-router-dom'; // Import the Link component
+import React, { useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 export const Navigation = () => {
+
+    const location = useLocation();
+
+  useEffect(() => {
+    const backButton = Telegram.WebApp.BackButton;
+
+    if (location.pathname !== '/') {
+      backButton.show();
+    } else {
+      backButton.hide();
+    }
+
+    backButton.onClick(() => {
+      history.back();
+    });
+
+    // Очистка обработчика при размонтировании компонента
+    return () => {
+      backButton.hide();
+    };
+  }, [location]);
+    
     return <div className="bottom-navigation">
         <div className="navigation-block">
            <Link to="/">Mining</Link>
