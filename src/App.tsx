28ac1660225e1c6ec222a8960a7c270f2e2dfc21
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-17 10:34:11 +0000

    kn

--- src/App.tsx
+++ src/App.tsx
@@ -14,41 +14,6 @@ import TelegramUser from "./components/TelegramUser/TelegramUser";
 import './telegram.d.ts';
 
 function App() {
-  useEffect(() => {
-    const loadScript = () => {
-      const script = document.createElement('script');
-      script.src = 'https://telegram.org/js/telegram-web-app.js';
-      script.async = true;
-      script.onload = () => {
-        if (window.Telegram && window.Telegram.WebApp) {
-          window.Telegram.WebApp.setHeaderColor('#ffffff');
-          window.Telegram.WebApp.expand();
-        }
-      };
-      document.body.appendChild(script);
-    };
-
-    loadScript();
-  }, []);
-
-  useEffect(() => {
-    const backButton = window.Telegram.WebApp.BackButton;
-
-    if (window.location.pathname !== '/') {
-      backButton.show();
-    } else {
-      backButton.hide();
-    }
-
-    backButton.onClick(() => {
-      window.history.back(); // Используем нативную JavaScript функцию для перехода назад
-    });
-
-    // Очистка обработчика при размонтировании компонента
-    return () => {
-      backButton.hide();
-    };
-  }, []);
 
   return (
     <TonConnectUIProvider
