2e2c6f7bbfea04fb7552f93eea53765debb78708
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-17 21:28:14 +0000

    l

--- src/pages/Market.tsx
+++ src/pages/Market.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './market.scss';
+import PageComponent from '../components/PageComponent/PageComponent';
 
 const Market: React.FC = () => {
-  return <div className="content"></div>;
+  return <PageComponent>
+      <div className="content"></div>
+  </PageComponent>;
 };
 
 export default Market;
