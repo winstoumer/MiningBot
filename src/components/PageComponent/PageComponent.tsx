5a4175a6e1c634131f6d7e152cf3da1bd8d5acd0
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-17 12:38:34 +0000

    gg

--- src/components/PageComponent/PageComponent.tsx
+++ src/components/PageComponent/PageComponent.tsx
@@ -1,28 +1,48 @@
 // components/PageComponent/PageComponent.tsx
 import React, { useEffect } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const PageComponent: React.FC = ({ children }) => {
-  const history = useHistory();
+
+  const location = useLocation();
+
+   useEffect(() => {
+    const loadScript = () => {
+      const script = document.createElement('script');
+      script.src = 'https://telegram.org/js/telegram-web-app.js';
+      script.async = true;
+      script.onload = () => {
+        if (window.Telegram && window.Telegram.WebApp) {
+          window.Telegram.WebApp.setHeaderColor('#ffffff');
+          window.Telegram.WebApp.expand();
+        }
+      };
+      document.body.appendChild(script);
+    };
+
+    loadScript();
+  }, []); 
+
   const location = useLocation();
 
   useEffect(() => {
     const backButton = window.Telegram.WebApp.BackButton;
 
-    if (location.pathname !== '/') {
+    if (window.location.pathname !== '/') {
       backButton.show();
     } else {
       backButton.hide();
     }
 
     backButton.onClick(() => {
-      history.goBack();
+      window.history.back(); // Используем нативную JavaScript функцию для перехода назад
     });
 
+    // Очистка обработчика при размонтировании компонента
     return () => {
       backButton.hide();
     };
-  }, [history, location]);
+  }, [location]);
 
   return <div>{children}</div>;
 };
--- src/pages/Boost.tsx
+++ src/pages/Boost.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './boost.scss';
 import PageComponent from '../components/PageComponent/PageComponent';
-import { useHistory } from 'react-router-dom'; // Добавить этот импорт
 
 type TelegramUserData = {
   id: number;
