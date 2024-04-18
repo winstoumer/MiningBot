53d037c1d09d5003dc2ef8389e2ece271927c0c6
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-18 21:27:09 +0000

    gh

--- src/pages/Market.tsx
+++ src/pages/Market.tsx
@@ -5,7 +5,7 @@ import { beginCell } from '@ton/ton'
 
 const body = beginCell()
   .storeUint(0, 32) // write 32 zero bits to indicate that a text comment will follow
-  .storeStringTail("Hello, TON!") // write our text comment
+  .storeStringTail("Hello, TON!") // write our text comment..
   .endCell();
 
 const myTransaction = {
