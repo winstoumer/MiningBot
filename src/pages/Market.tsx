c97bd340439023676c34b039390cbb9fb4e3af41
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-18 08:48:11 +0000

    h

--- src/pages/Market.tsx
+++ src/pages/Market.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './market.scss';
 import PageComponent from '../components/PageComponent/PageComponent';
+import TxForm from "./components/TxForm/TxForm";
 
 const Market: React.FC = () => {
   return <PageComponent>
+      <TxForm />
       <div className="content">
           <button type="button" className="button">Collecting</button>
       </div>
