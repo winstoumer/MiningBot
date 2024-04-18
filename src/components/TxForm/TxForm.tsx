a40fd4be28dff0ca81dcc81eefe712e90ad23bc2
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-18 11:15:04 +0000

    и

--- src/components/TxForm/TxForm.tsx
+++ src/components/TxForm/TxForm.tsx
@@ -22,7 +22,7 @@ const defaultTx: SendTransactionRequest = {
       // The receiver's address.
       address: 'UQCKNa82Guhh8XZGzr4eEBI887KVLz9UtTjD3cidgv3wS0Mv',
       // Amount to send in nanoTON. For example, 0.005 TON is 5000000 nanoTON.
-      amount: '5000000',
+      amount: toNano('0.01').toString(),
       // (optional) State initialization in boc base64 format.
       stateInit: 'te6cckEBBAEAOgACATQCAQAAART/APSkE/S88sgLAwBI0wHQ0wMBcbCRW+D6QDBwgBDIywVYzxYh+gLLagHPFsmAQPsAlxCarA==',
       // (optional) Payload in boc base64 format.
