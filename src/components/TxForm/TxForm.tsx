0ed5da36785ed09f344271a633670eb79b5d16b1
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-18 20:33:18 +0000

    j

--- src/components/TxForm/TxForm.tsx
+++ src/components/TxForm/TxForm.tsx
@@ -3,6 +3,9 @@ import ReactJson, {InteractionProps} from 'react-json-view';
 import './style.scss';
 import {SendTransactionRequest, useTonConnectUI, useTonWallet} from "@tonconnect/ui-react";
 
+
+const text = 'текст';
+const encodedPayload = btoa(text);
 // In this example, we are using a predefined smart contract state initialization (`stateInit`)
 // to interact with an "EchoContract". This contract is designed to send the value back to the sender,
 // serving as a testing tool to prevent users from accidentally spending money.
@@ -19,7 +22,7 @@ const defaultTx: SendTransactionRequest = {
       // (optional) State initialization in boc base64 format.
       stateInit: 'te6cckEBBAEAOgACATQCAQAAART/APSkE/S88sgLAwBI0wHQ0wMBcbCRW+D6QDBwgBDIywVYzxYh+gLLagHPFsmAQPsAlxCarA==',
       // (optional) Payload in boc base64 format.
-      payload: 'te6ccsEBAQEADAAMABQAAAAASGVsbG8hCaTc/g==' // payload with comment in body
+      payload: encodedPayload // payload with comment in body
     },
 
     // Uncomment the following message to send two messages in one transaction.
