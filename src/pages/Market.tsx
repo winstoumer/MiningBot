2aba9fa85e296f00b8c5184a5d62c2856cd64ab0
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-18 21:40:04 +0000

    c

--- src/pages/Market.tsx
+++ src/pages/Market.tsx
@@ -15,18 +15,6 @@ import {beginCell, toNano, Address} from '@ton/ton'
         .storeStringTail('Hello, TON!')
         .endCell();
 
-    const body = beginCell()
-        .storeUint(0x0f8a7ea5, 32) // opcode for jetton transfer
-        .storeUint(0, 64) // query id
-        .storeCoins(toNano(5)) // jetton amount, amount * 10^9
-        .storeAddress(destinationAddress) // TON wallet destination address
-        .storeAddress(destinationAddress) // response excess destination
-        .storeBit(0) // no custom payload
-        .storeCoins(toNano('0.02')) // forward amount (if >0, will send notification message)
-        .storeBit(1) // we store forwardPayload as a reference
-        .storeRef(forwardPayload)
-        .endCell();
-
 const body = beginCell()
   .storeUint(0, 32) // write 32 zero bits to indicate that a text comment will follow
   .storeStringTail("Hello, TON!") // write our text comment..
