f192063ad1ffd5aec304a3e55aa5b5628fff0f33
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-18 21:53:31 +0000

    gg

--- src/pages/Market.tsx
+++ src/pages/Market.tsx
@@ -1,34 +1,11 @@
 import React from 'react';
 import './market.scss';
-import { useTonConnectUI } from '@tonconnect/ui-react';
-import { beginCell, toNano, Address } from '@ton/ton';
-
-const destinationAddress = 'UQCKNa82Guhh8XZGzr4eEBI887KVLz9UtTjD3cidgv3wS0Mv';
-
-const body = beginCell()
-  .storeUint(0, 32)
-  .storeStringTail("Hello, TON!")
-  .endCell();
-
-const myTransaction = {
-  validUntil: Math.floor(Date.now() / 1000) + 360,
-  messages: [
-    {
-      address: destinationAddress,
-      amount: toNano("0.05").toString(),
-      payload: body.toBoc().toString("base64")
-    }
-  ]
-};
 
 const Market: React.FC = () => {
-  const [tonConnectUI, setOptions] = useTonConnectUI();
 
   return (
     <div>
-      <button onClick={() => tonConnectUI.sendTransaction(myTransaction)}>
-        Send transaction
-      </button>
+    
     </div>
   );
 };
