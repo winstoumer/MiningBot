2ed84ed4c2b13775ad2fb40e3917c3504fa334e7
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-18 21:25:52 +0000

    h

--- src/pages/Market.tsx
+++ src/pages/Market.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import './market.scss';
-import PageComponent from '../components/PageComponent/PageComponent';
+import { useTonConnectUI } from '@tonconnect/ui-react';
+import { beginCell } from '@ton/ton'
+
+const body = beginCell()
+  .storeUint(0, 32) // write 32 zero bits to indicate that a text comment will follow
+  .storeStringTail("Hello, TON!") // write our text comment
+  .endCell();
+
+const myTransaction = {
+    validUntil: Math.floor(Date.now() / 1000) + 360,
+    messages: [
+        {
+            address: 'UQCKNa82Guhh8XZGzr4eEBI887KVLz9UtTjD3cidgv3wS0Mv',
+            amount: toNano("0.05"),
+            payload: body.toBoc().toString("base64") // payload with comment in body
+        }
+    ]
+}
 
 const Market: React.FC = () => {
-  return <PageComponent>
-  </PageComponent>;
+  const [tonConnectUI, setOptions] = useTonConnectUI();
+
+    return (
+        <div>
+            <button onClick={() => tonConnectUI.sendTransaction(myTransaction)}>
+                Send transaction
+            </button>
+        </div>
+    );
 };
 
 export default Market;
