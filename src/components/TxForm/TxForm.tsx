a1b25d3db31770c396d85e339b5a23ac315adaf7
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-18 20:37:22 +0000

    п

--- src/components/TxForm/TxForm.tsx
+++ src/components/TxForm/TxForm.tsx
@@ -1,49 +1,28 @@
-import React, {useCallback, useState} from 'react';
-import ReactJson, {InteractionProps} from 'react-json-view';
+import React, { useCallback, useState } from 'react';
+import ReactJson, { InteractionProps } from 'react-json-view';
 import './style.scss';
-import {SendTransactionRequest, useTonConnectUI, useTonWallet} from "@tonconnect/ui-react";
-
-
-const text = 'текст';
-const encodedPayload = btoa(text);
-// In this example, we are using a predefined smart contract state initialization (`stateInit`)
-// to interact with an "EchoContract". This contract is designed to send the value back to the sender,
-// serving as a testing tool to prevent users from accidentally spending money.
-const defaultTx: SendTransactionRequest = {
-  // The transaction is valid for 10 minutes from now, in unix epoch seconds.
-  validUntil: Math.floor(Date.now() / 1000) + 600,
-  messages: [
-
-    {
-      // The receiver's address.
-      address: 'UQCKNa82Guhh8XZGzr4eEBI887KVLz9UtTjD3cidgv3wS0Mv',
-      // Amount to send in nanoTON. For example, 0.005 TON is 5000000 nanoTON.
-      amount: '5000000',
-      // (optional) State initialization in boc base64 format.
-      stateInit: 'te6cckEBBAEAOgACATQCAQAAART/APSkE/S88sgLAwBI0wHQ0wMBcbCRW+D6QDBwgBDIywVYzxYh+gLLagHPFsmAQPsAlxCarA==',
-      // (optional) Payload in boc base64 format.
-      payload: encodedPayload /// payload with comment in body
-    },
-
-    // Uncomment the following message to send two messages in one transaction.
-    
-    //{
-      // Note: Funds sent to this address will not be returned back to the sender.
-      //address: 'UQCKNa82Guhh8XZGzr4eEBI887KVLz9UtTjD3cidgv3wS0Mv',
-      //amount: toNano('0.01').toString(),
-      //payload: body.toBoc().toString("base64")
-    //}
-    
-
-  ],
-};
+import { SendTransactionRequest, useTonConnectUI, useTonWallet } from "@tonconnect/ui-react";
 
 export function TxForm() {
+  // Генерация закодированного payload
+  const text = 'текст';
+  const encodedPayload = btoa(text);
+
+  const defaultTx: SendTransactionRequest = {
+    validUntil: Math.floor(Date.now() / 1000) + 600,
+    messages: [
+      {
+        address: 'UQCKNa82Guhh8XZGzr4eEBI887KVLz9UtTjD3cidgv3wS0Mv',
+        amount: '5000000',
+        stateInit: 'te6cckEBBAEAOgACATQCAQAAART/APSkE/S88sgLAwBI0wHQ0wMBcbCRW+D6QDBwgBDIywVYzxYh+gLLagHPFsmAQPsAlxCarA==',
+        // Добавление закодированного payload
+        payload: encodedPayload,
+      },
+    ],
+  };
 
   const [tx, setTx] = useState(defaultTx);
-
   const wallet = useTonWallet();
-
   const [tonConnectUi] = useTonConnectUI();
 
   const onChange = useCallback((value: InteractionProps) => {
@@ -53,9 +32,7 @@ export function TxForm() {
   return (
     <div className="send-tx-form">
       <h3>Configure and send transaction</h3>
-
       <ReactJson theme="ocean" src={defaultTx} onEdit={onChange} onAdd={onChange} onDelete={onChange}/>
-
       {wallet ? (
         <button onClick={() => tonConnectUi.sendTransaction(tx)}>
           Send transaction
