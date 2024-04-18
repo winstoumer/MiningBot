91143002057e4e4ae706c28b44faf90762707951
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-18 21:46:08 +0000

    р

--- src/pages/Market.tsx
+++ src/pages/Market.tsx
@@ -1,42 +1,36 @@
-import React from 'react'
+import React from 'react';
 import './market.scss';
 import { useTonConnectUI } from '@tonconnect/ui-react';
+import { beginCell, toNano, Address } from '@ton/ton';
 
-import {beginCell, toNano, Address} from '@ton/ton'
-    // transfer#0f8a7ea5 query_id:uint64 amount:(VarUInteger 16) destination:MsgAddress
-    // response_destination:MsgAddress custom_payload:(Maybe ^Cell)
-    // forward_ton_amount:(VarUInteger 16) forward_payload:(Either Cell ^Cell)
-    // = InternalMsgBody;
-
-    const destinationAddress = Address.parse('UQCKNa82Guhh8XZGzr4eEBI887KVLz9UtTjD3cidgv3wS0Mv');
-
+const destinationAddress = 'UQCKNa82Guhh8XZGzr4eEBI887KVLz9UtTjD3cidgv3wS0Mv';
 
 const body = beginCell()
-  .storeUint(0, 32) // write 32 zero bits to indicate that a text comment will follow
-  .storeStringTail("Hello, TON!") // write our text comment..
+  .storeUint(0, 32)
+  .storeStringTail("Hello, TON!")
   .endCell();
 
 const myTransaction = {
-    validUntil: Math.floor(Date.now() / 1000) + 360,
-    messages: [
-        {
-            address: destinationAddress,
-            amount: toNano("0.05"),
-            payload: body.toBoc().toString("base64") // payload with comment in body
-        }
-    ]
-}
+  validUntil: Math.floor(Date.now() / 1000) + 360,
+  messages: [
+    {
+      address: destinationAddress,
+      amount: toNano("0.05").toString(),
+      payload: body.toBoc().toString("base64")
+    }
+  ]
+};
 
 const Market: React.FC = () => {
   const [tonConnectUI, setOptions] = useTonConnectUI();
 
-    return (
-        <div>
-            <button onClick={() => tonConnectUI.sendTransaction(myTransaction)}>
-                Send transaction
-            </button>
-        </div>
-    );
+  return (
+    <div>
+      <button onClick={() => tonConnectUI.sendTransaction(myTransaction)}>
+        Send transaction
+      </button>
+    </div>
+  );
 };
 
 export default Market;
