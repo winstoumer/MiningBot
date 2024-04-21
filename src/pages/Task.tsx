8b742dce985375dfe14f17909a927dae5a259682
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-21 15:36:02 +0000

    g

--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -117,24 +117,6 @@ const Task: React.FC = () => {
     );
 }, []);
 
-
-  useEffect(() => {
-    const loadScript = () => {
-      const script = document.createElement('script');
-      script.src = 'https://telegram.org/js/telegram-web-app.js';
-      script.async = true;
-      script.onload = () => {
-        if (window.Telegram && window.Telegram.WebApp) {
-            window.Telegram.WebApp.setHeaderColor('#ffffff');
-          window.Telegram.WebApp.expand();
-        }
-      };
-      document.body.appendChild(script);
-    };
-
-    loadScript();
-  }, []);
-
   useEffect(() => {
     if (window.Telegram && window.Telegram.WebApp) {
       setUserData(window.Telegram.WebApp.initDataUnsafe?.user);
