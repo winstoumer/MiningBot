0916c53e9753f0825cee69379ffed84c32a55df5
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-21 15:38:03 +0000

    cf

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -37,23 +37,6 @@ const Home: React.FC = () => {
     const [startCoins, setStartCoins] = useState<number>(0.00000000); // Начальное значение для счетчика монет
 
   useEffect(() => {
-    const loadScript = () => {
-      const script = document.createElement('script');
-      script.src = 'https://telegram.org/js/telegram-web-app.js';
-      script.async = true;
-      script.onload = () => {
-        if (window.Telegram && window.Telegram.WebApp) {
-          window.Telegram.WebApp.setHeaderColor('#000000');
-          window.Telegram.WebApp.expand();
-        }
-      };
-      document.body.appendChild(script);
-    };
-
-    loadScript();
-  }, []);
-
-  useEffect(() => {
     if (window.Telegram && window.Telegram.WebApp) {
       setUserData(window.Telegram.WebApp.initDataUnsafe?.user);
     }
