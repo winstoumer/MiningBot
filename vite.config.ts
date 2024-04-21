06f7ce5271d8428f252e01ea1c7ae53ed4c4f4d3
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-21 19:45:31 +0000

    cx

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// nodePolyfills
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 
