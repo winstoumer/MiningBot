0d0c0b66cbcd8ce9b6fe4e7f8babd91c635f7b16
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-21 12:54:30 +0000

    р

--- src/pages/Box.tsx
+++ src/pages/Box.tsx
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './box.scss';
 import PageComponent from '../components/PageComponent/PageComponent';
 
-
 const Box: React.FC = () => {
-    return 
+    return (
         <PageComponent>
             <div>test2</div>
         </PageComponent>
+    );
 };
+
 export default Box;
