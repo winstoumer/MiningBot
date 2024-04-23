da3f67e718d3a854bbaec62d80d59473c040eae7
winstoumer <kwt56zrv8b@privaterelay.appleid.com>
2024-04-23 10:33:52 +0000

    h

--- src/pages/Box.tsx
+++ src/pages/Box.tsx
@@ -194,8 +194,8 @@ const decrementTotal = async (userId: number) => {
             <PageComponent>
                 <div className="box-container">
                     <div className="box-body">
-                        <div className="count-box">{boxData?.total ?? '•'}</div>
-                        <div className="watch-box">
+                        <div className="count-box">{boxData?.total ?? ''}</div>
+                        <div className={"watch-box" + (boxData?.total ? "" : " hide-box")}>
                             <img src="https://i.ibb.co/jLcwk8W/IMG-1679.jpg" className="box-image" alt="box" />
                         </div>
                         <div className="box-rewards">
